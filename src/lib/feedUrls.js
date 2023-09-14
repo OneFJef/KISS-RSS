c7a4bc4e57e3123615a4c851aafd6d60a9f56df7
Jef Mitchell <jef@sentient-modem.com>
2023-09-13 23:21:56 -0500

    Feed: https://lemm.ee/feeds/all.xml?sort=TopWeek

--- src/lib/feedUrls.js
+++ src/lib/feedUrls.js
@@ -2,5 +2,6 @@
 
 export let feedUrls = [
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCaIG92A013x_Cuv-yQOO7Hw",
-    "https://botsin.space/@picardtips.rss"
+    "https://botsin.space/@picardtips.rss",
+    "https://lemm.ee/feeds/all.xml?sort=TopWeek"
 ];
