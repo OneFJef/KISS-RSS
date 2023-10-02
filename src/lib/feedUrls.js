83a3870694516ef65a411ee50a5f17cf198459a7
Jef Mitchell <jef@sentient-modem.com>
2023-10-02 08:21:10 -0500

    Updated incorrect URL formating.

--- src/lib/feedUrls.js
+++ src/lib/feedUrls.js
@@ -1,6 +1,6 @@
 //  Youtube RSS URL - https://www.youtube.com/feeds/videos.xml?channel_id=
 
 export let feedUrls = [
-    "https://www.youtube.com/feeds/videos.xml?channel_id=UCaIG92A013x_Cuv-yQOO7Hw", // MFPallyTime
-    "https://www.youtube.com/channel/UCuQbvx2ShWp08qG7N9QZLBg" // JLKgg
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCaIG92A013x_Cuv-yQOO7Hw",
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCuQbvx2ShWp08qG7N9QZLBg"
 ];
