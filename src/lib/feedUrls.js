1bb062b32c55673a7057e3b0bb22379f069b2a9f
Jef Mitchell <jef@sentient-modem.com>
2023-10-02 02:44:51 -0500

    Added URL

--- src/lib/feedUrls.js
+++ src/lib/feedUrls.js
@@ -1,7 +1,6 @@
 //  Youtube RSS URL - https://www.youtube.com/feeds/videos.xml?channel_id=
 
 export let feedUrls = [
-    "https://www.youtube.com/feeds/videos.xml?channel_id=UCaIG92A013x_Cuv-yQOO7Hw",
-    "https://botsin.space/@picardtips.rss",
-    "https://lemm.ee/feeds/all.xml?sort=TopWeek"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCaIG92A013x_Cuv-yQOO7Hw", // MFPallyTime
+    "https://www.youtube.com/channel/UCuQbvx2ShWp08qG7N9QZLBg" // JLKgg
 ];
